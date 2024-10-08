9a4556a04f18af7672031dcfcfa5bcd5135f78da
Tomoe-12 <li8993han@gmail.com>
2024-10-08 09:24:23 +0630

    multi selector and why shallow

--- src/components/CatController.tsx
+++ src/components/CatController.tsx
@@ -1,10 +1,26 @@
-import React from "react";
 import useCatStore from "../stores/catStore";
+import { shallow } from "zustand/shallow";
 
 const CatController = () => {
-//   const {  increaseBigCats, increaseSmallCats } = useCatStore();
-    const increaseBigCats = useCatStore.use.increaseBigCats()
-    const increaseSmallCats = useCatStore.use.increaseSmallCats()
+  //   const {  increaseBigCats, increaseSmallCats } = useCatStore();
+
+  const increaseBigCats = useCatStore.use.increaseBigCats()
+  const increaseSmallCats = useCatStore.use.increaseSmallCats()
+  // instead of these two write these instead
+
+//    from two writing style is a multi selector but still got error i donna know why let me fix it next time 
+  // const {increaseBigCats , increaseSmallCats} = useCatStore(
+  //     (state) => ({
+  //         increaseBigCats: state.increaseBigCats,
+  //         increaseSmallCats : state.increaseSmallCats
+  //     }),
+  //     shallow
+  // )
+
+//   const [increaseBigCats, increaseSmallCats] = useCatStore(
+//     (state) => [state.increaseBigCats, state.increaseSmallCats],
+//     shallow
+//   );
 
   return (
     <div className="box">
